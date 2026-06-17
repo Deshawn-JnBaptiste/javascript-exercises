6c34151a7400b2b8d921c27fa3bb8a5dd097a6bd
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-17 16:02:58 -0400

    Add empty string variable to hold all repeated strings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,10 +1,13 @@
 const repeatString = function(str, num) {
     if (num < 0) {
-        return "ERROR";s
+        return "ERROR";
     }
-    for (let i = 0; i < num; i++) [
+    let string = "";
 
-    ]
+    for (let i = 0; i < num; i++) {
+        string += str;
+    }
+    return string;
 };
 
 // Do not edit below this line
