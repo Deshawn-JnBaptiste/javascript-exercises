35862175332c4711d69783024063f27b00623a9c
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-19 11:32:09 -0400

    Check if year is divisible by 100 and if it is return false else return true

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,7 +1,11 @@
 const leapYears = function(year) {
 //Check if year is divisble by 4 then check if it is divisble by 100
 if (year % 4 == 0) {
-
+    if (year % 100 == 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 };
