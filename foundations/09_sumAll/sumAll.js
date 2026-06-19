420dc64e8b6724c6d792fd935ee02d6606d77444
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-18 22:48:49 -0400

    If either numbers are not intergers return with an error message

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,8 +1,9 @@
 const sumAll = function(num1, num2) {
 
-    if (num1 <= 0 || num2 <= 0) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
     return "ERROR";
-}
+    }
+
 
     let sum = 0;
     if (num1 < num2) {
