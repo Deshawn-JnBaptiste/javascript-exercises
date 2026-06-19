767267acb1033df07df7b0fec03fb07835046c88
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-19 12:58:04 -0400

    Add the formulae used to calculate both °F to °C and °C to °F

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  // fahrenheit to celsius
+  return (num - 32) / 1.8;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) { 
+  // celsius to fahrenheit
+  return (num * 1.8) + 32;
 };
 
 // Do not edit below this line
