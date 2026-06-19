a2688fa958bde56a08d942a69f560edb6a80adf7
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-18 22:52:10 -0400

    If either numbers are less than or equal to 0 return with an error message

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,9 +1,11 @@
 const sumAll = function(num1, num2) {
 
     if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
-    return "ERROR";
+        return "ERROR";
     }
-
+    if (num1 <= 0 || num2 <= 0) {
+        return "ERROR"
+    }
 
     let sum = 0;
     if (num1 < num2) {
