2130665e0165b4d7c15c82f9f10211446193736c
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-19 13:08:09 -0400

    Use math.round to round the result to one decimal place

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
 const convertToCelsius = function(num) {
   // fahrenheit to celsius
-  return (num - 32) / 1.8;
+  let cel = (num - 32) / 1.8;
+  return Math.round(cel * 10) / 10;
 
 };
 
 const convertToFahrenheit = function(num) { 
   // celsius to fahrenheit
-  return (num * 1.8) + 32;
+  let fah = (num * 1.8) + 32;
+  return Math.round(fah * 10) / 10;
 };
 
 // Do not edit below this line
