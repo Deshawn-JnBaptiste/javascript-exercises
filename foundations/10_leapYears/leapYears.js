b8acf69f8935b9cd512d16441c782379fd14a602
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-19 12:26:54 -0400

    Add check for if year is divisible by 400 after first checking if it is divisble by 100

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -2,12 +2,17 @@ const leapYears = function(year) {
 
 if (year % 4 == 0) {
     if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
         return false;
-    } else {
+        }
+        } else {
         return true;
     }
-}
+} else {
 return false;
+}
 
 };
 
