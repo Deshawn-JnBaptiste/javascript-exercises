04f180aef66ff490bee2c465f008849e6bb5ad0d
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-07-27 15:04:54 -0400

    Create check for numbers 1 and 2 as given index

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,7 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
    let previous = 1;
    let current = 1;
    let next;
+
+   if (index == 1 || index == 2) {
+    return 1;
+   }
+
+   for(let i = 0; i < index; i++) {
+    next = previous + current;
+    previous = current;
+    current = next;
+   }
+   return next;
 };
 
 // Do not edit below this line
