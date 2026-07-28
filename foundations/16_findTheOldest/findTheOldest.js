5c566507d4b617d61acfdfae67bb1d9ff0278965
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-07-28 10:18:58 -0400

    Create a new array with calculated ages

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const newPeople = arr.map((person) => {
 
+        const newPerson = {
+            name: person.name,
+            yearOfBirth: person.yearOfBirth,
+            yearOfDeath: person.yearOfDeath,
+        }
+        
+        if (!Object.hasOwn(person, "yearOfDeath")) {
+            const currentDate = new Date().getFullYear();
+            newPerson.age = currentDate - person.yearOfBirth
+        } else {
+            newPerson.age = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        return newPerson
+    })
 };
 
 // Do not edit below this line
